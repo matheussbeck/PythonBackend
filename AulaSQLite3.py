d92ea5c730979902d5e58c5a1719bdc37580a6bb
Matheus Beck <matheussbeck@outlook.com>
2024-08-17 12:57:27 -0300

    AulaSQLite3

--- AulaSQLite3.py
+++ AulaSQLite3.py
@@ -4,6 +4,7 @@ from pathlib import Path
 ROOT_PATH = Path(__file__).parent
 conexao = sqlite3.connect(ROOT_PATH / "meubanco.db")
 cursor = conexao.cursor()
+cursor.row_factory = sqlite3.Row
 
 def criar_tabela(conexao,cursor):
     cursor.execute ('CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VACHAR(100), email VARCHAR(150))')
@@ -28,5 +29,28 @@ def excluir_registro(conexao,cursor,id):
     conexao.commit()
 
 
-excluir_registro(conexao, cursor,1)
+def inserir_lote_registros(conexao,cursor,dados):
+    cursor.executemany('INSERT INTO clientes (nome, email) VALUES (?,?)', dados)
+    conexao.commit()
 
+
+def mostrar_registro(conexao,cursor,id):
+    try:
+        cursor.execute('SELECT * FROM clientes WHERE id=?',dados)
+        return cursor.fetchone()
+    
+
+def mostrar_todos_alfabetico(cursor):
+    return cursor.execute('SELECT * FROM clientes ORDER BY nome DESC;')
+
+
+
+cursor.execute('SELECT * FROM clientes WHERE id = 1')
+
+clientes = mostrar_todos_alfabetico(cursor)
+for cliente in clientes:
+    print(dict(cliente))
+
+cliente = mostrar_registro(cursor, 2)
+print(dict(cliente))
+print(cliente["id"])
