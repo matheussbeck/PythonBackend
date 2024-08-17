7e3b1ea8eb4c127f5d47451c8f1691e8fd3204e7
Matheus Beck <matheussbeck@outlook.com>
2024-08-17 11:23:48 -0300

    Aula SQLite3

--- AulaSQLite3.py
+++ AulaSQLite3.py
@@ -2,7 +2,31 @@ import sqlite3
 from pathlib import Path
 
 ROOT_PATH = Path(__file__).parent
+conexao = sqlite3.connect(ROOT_PATH / "meubanco.db")
+cursor = conexao.cursor()
 
-conexao = sqlite3.connect(ROOT_PATH / "clientes.db")
+def criar_tabela(conexao,cursor):
+    cursor.execute ('CREATE TABLE clientes (id INTEGER PRIMARY KEY AUTOINCREMENT, nome VACHAR(100), email VARCHAR(150))')
+    conexao.commit()
 
-print(conexao)
+
+def inserir_registro(conexao,cursor,nome,email):
+    data = (nome,email)
+    cursor.execute (f"INSERT INTO clientes(nome,email) VALUES(?,?)",data)
+    conexao.commit()
+
+
+def atualizar_registro(conexao,cursor,nome,email,id):
+    data = (nome,email,id)
+    cursor.execute('UPDATE clientes SET nome=?, email=? WHERE id=?;', data)
+    conexao.commit()
+
+
+def excluir_registro(conexao,cursor,id):
+    data = (id,)
+    cursor.execute('DELETE FROM clientes WHERE id=?;', data)
+    conexao.commit()
+
+
+excluir_registro(conexao, cursor,1)
+
